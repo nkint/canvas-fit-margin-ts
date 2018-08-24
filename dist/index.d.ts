5c1fddd298009d803f6795cf9d61f844aaa32a99
alberto <alberto.massa@ymail.com>
2018-08-24 16:02:47 +0200

    definitions in  /dist

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare type CanvasFitOptions = {
+    parent?: HTMLElement | Window | Node;
+    margin?: number;
+    scale?: number;
+};
+export declare function createFit(canvas: HTMLCanvasElement, options?: CanvasFitOptions): () => any;
+//# sourceMappingURL=index.d.ts.map
